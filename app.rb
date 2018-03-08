8c6c554998a650aadc650b8fc226d4e53903fd50
Ignazio Calò <icalo@ebay.com>
2018-03-08 10:42:33 +0100

    First commit

---
+++ app.rb
@@ -0,0 +1,69 @@
+require 'rubygems'
+require 'bundler'
+require 'fileutils'
+Bundler.require()
+
+require "sinatra/reloader"
+require_relative "generator.rb"
+
+class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    after_reload do
+      puts 'reloaded'
+    end
+  end
+
+  enable :methodoverride
+  set :bind, '0.0.0.0'
+  set :port, 8080
+
+
+  post '/' do
+    @line1 = params[:line1]
+    @line2 = params[:line2]
+    @line3 = params[:line3]
+    puts params
+    color = colorMap[params[:color]]
+
+    Dir.glob('*.png').each { |file| File.delete(file) }
+
+    sentences = []
+    sentences << @line1.upcase if @line1.length > 0
+    sentences << @line2.upcase if @line2.length > 0
+    #sentences << @line3.upcase if @line3.length > 0
+    puts "*** #{sentences}"
+    gg = Generator.new(random_name)
+    @generated_name = gg.generate(sentences, color)
+    FileUtils.cp(@generated_name, File.join('public', @generated_name))
+
+    # erb :index
+    fullText = sentences.join(" ")
+    filename = sanitize_filename(fullText)
+    send_file "./public/#{@generated_name}", :filename => "#{filename}.png", :type => 'Application/image'
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  def colorMap
+    map = Hash.new("rgb(135, 185, 25)")
+    map["green"] = "rgb(135, 185, 25)"
+    map["red"] = "rgb(229, 50, 56)"
+    map["blue"] = "rgb(0, 98, 212)"
+    map
+  end
+
+  def random_name
+    (0...8).map { (65 + rand(26)).chr }.join
+  end
+
+  def sanitize_filename(filename)
+    clean_name = filename.gsub(/\s+/, '_')
+    clean_name.gsub!(/[^0-9A-Za-z.\-]/, '_')
+    clean_name
+  end
+end
+
+App.run!
---
+++ generator.rb
@@ -0,0 +1,59 @@
+class Generator
+  attr_accessor :prefix
+
+  def initialize(prefix)
+    self.prefix = prefix
+  end
+
+  def generate(lines, color = "rgb(135, 185, 25)")
+    puts "This is the prefix #{self.prefix}"
+
+    lines.each_with_index do | line, index |
+      margin = ""
+      if index == 0
+        puts "TOP"
+        margin = "-gravity north -splice 0x10 -gravity south -splice 0x4"
+      elsif index == lines.count - 1
+        puts "BOTTOM"
+        margin = "-gravity north -splice 0x11 -gravity south -splice 0x15"
+      end
+
+      command = <<-HEREDOC
+      convert -background "#{color}" \
+        -size 10000x290 \
+        -gravity Center \
+        -fill white \
+        -font HelveticaNeueCondensedBlack.ttf \
+        -pointsize 95 \
+        caption:"#{line}" \
+        -trim \
+        -bordercolor "#{color}" \
+        -border 17x5 \
+        -background "#{color}" #{margin}\
+        #{self.prefix}_base_#{index}.png
+        HEREDOC
+      system( command )
+    end
+    merge
+    return skew
+  end
+
+
+
+
+  def merge
+    command = "convert #{self.prefix}_base_*.png -background none -gravity Center -append #{self.prefix}_merged.png"
+    system( command )
+  end
+
+  def skew()
+    filename = "final_#{self.prefix}.png"
+    command = "./skew.sh -a 6 -m degrees -d r2t #{self.prefix}_merged.png #{filename}"
+    system ( command )
+    return filename
+  end
+
+  def random_name
+    (0...8).map { (65 + rand(26)).chr }.join
+  end
+end
